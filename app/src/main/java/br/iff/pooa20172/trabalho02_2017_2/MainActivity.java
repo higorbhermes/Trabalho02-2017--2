87f7fda7e4abdf65ffff453a1bcbcea45e2efbd6
higorbhermes <higorbhermes@gmail.com>
2017-11-29 11:52:07 -0200

    ListView

--- app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/MainActivity.java
+++ app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/MainActivity.java
@@ -2,12 +2,19 @@ package br.iff.pooa20172.trabalho02_2017_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lista = (ListView) findViewById(R.id.lvEquipes);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.equipes,android.R.layout.simple_list_item_1);
+
+        lista.setAdapter(adapter);
     }
 }
