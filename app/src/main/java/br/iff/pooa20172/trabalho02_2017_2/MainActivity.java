afa449ada2a9cda3042a47041677229f08f40839
higorbhermes <higorbhermes@gmail.com>
2017-11-30 00:45:27 -0200

    Itens na Tela Principal

--- app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/MainActivity.java
+++ app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/MainActivity.java
@@ -5,16 +5,32 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ListView lista = (ListView) findViewById(R.id.lvEquipes);
 
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.equipes,android.R.layout.simple_list_item_1);
+        final ArrayList<Evento> evento = adicionarEvento();
+
+        ArrayAdapter adapter = new EventoAdapter(this, evento);
+        lista.setAdapter(adapter);
 
         lista.setAdapter(adapter);
+    }
+
+    private ArrayList<Evento> adicionarEvento() {
+        ArrayList<Evento> evento = new ArrayList<Evento>();
+        Evento e = new Evento("Nome do Evento", R.drawable.nome);
+        evento.add(e);
+        Evento f = new Evento("Data do Evento", R.drawable.data);
+        evento.add(f);
+        Evento g = new Evento("Local do Evento", R.drawable.local);
+        evento.add(g);
+        return evento;
     }
 }
