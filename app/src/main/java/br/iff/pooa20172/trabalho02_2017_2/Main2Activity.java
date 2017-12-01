dfda3e8364fc784a8281ae96eeab9e54791b8e6e
higorbhermes <higorbhermes@gmail.com>
2017-12-01 12:55:48 -0200

    StartActivityForResult e OnFinish

--- app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/Main2Activity.java
+++ app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/Main2Activity.java
@@ -1,14 +1,65 @@
 package br.iff.pooa20172.trabalho02_2017_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        EditText et_Nome = (EditText) findViewById(R.id.et_Nome);
+        EditText et_Data = (EditText) findViewById(R.id.et_Data);
+        EditText et_Local = (EditText) findViewById(R.id.et_Local);
+        EditText et_Capacidade = (EditText) findViewById(R.id.et_Capacidade);
+        EditText et_Promotor = (EditText) findViewById(R.id.et_Promotor);
+        EditText et_Patrocinio = (EditText) findViewById(R.id.et_Patrocinio);
+        EditText et_Valor = (EditText) findViewById(R.id.et_Valor);
+
+        Intent intent = getIntent();
+
+        String nome = (String) intent.getSerializableExtra("nome");
+        String data = (String) intent.getSerializableExtra("data");
+        String local = (String) intent.getSerializableExtra("local");
+        int capacidade = (int) intent.getSerializableExtra("capacidade");
+        String promotor = (String) intent.getSerializableExtra("promotor");
+        String patrocinio = (String) intent.getSerializableExtra("patrocinio");
+        double valor = (double) intent.getSerializableExtra("valor");
+
+        et_Nome.setText(nome);
+        et_Data.setText(data);
+        et_Local.setText(local);
+        et_Capacidade.setText(Integer.toString(capacidade));
+        et_Promotor.setText(promotor);
+        et_Patrocinio.setText(patrocinio);
+        et_Valor.setText(Double.toString(valor));
+    }
+
+    public void finish() {
+        EditText et_Nome = (EditText) findViewById(R.id.et_Nome);
+        EditText et_Data = (EditText) findViewById(R.id.et_Data);
+        EditText et_Local = (EditText) findViewById(R.id.et_Local);
+        EditText et_Capacidade = (EditText) findViewById(R.id.et_Capacidade);
+        EditText et_Promotor = (EditText) findViewById(R.id.et_Promotor);
+        EditText et_Patrocinio = (EditText) findViewById(R.id.et_Patrocinio);
+        EditText et_Valor = (EditText) findViewById(R.id.et_Valor);
+
+        Intent intent = new Intent();
+
+        intent.putExtra("nome", et_Nome.getText());
+        intent.putExtra("data", et_Data.getText());
+        intent.putExtra("local", et_Local.getText());
+        intent.putExtra("capacidade", et_Capacidade.getText());
+        intent.putExtra("promotor", et_Promotor.getText());
+        intent.putExtra("patrocinio", et_Patrocinio.getText());
+        intent.putExtra("valor", et_Valor.getText());
+        intent.putExtra("retorno", "Retornou");
+        setResult(RESULT_OK, intent);
+        super.finish();
     }
 
 }
--- app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/MainActivity.java
+++ app/src/main/java/br/iff/pooa20172/trabalho02_2017_2/MainActivity.java
@@ -12,6 +12,9 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_CONFIRMACAO = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,17 +34,49 @@ public class MainActivity extends AppCompatActivity {
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
             Intent intent = new Intent(MainActivity.this, Main2Activity.class);
             intent.putExtra("nome", evento.get(i).getNome());
-            startActivity(intent);
+            intent.putExtra("data", evento.get(i).getData());
+            intent.putExtra("local", evento.get(i).getLocal());
+            intent.putExtra("capacidade", evento.get(i).getCapacidade());
+            intent.putExtra("promotor", evento.get(i).getPromotor());
+            intent.putExtra("patrocinio", evento.get(i).getPatrocinio());
+            intent.putExtra("valor", evento.get(i).getValor());
+            startActivityForResult(intent, REQUEST_CODE_CONFIRMACAO);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent dados) {
+        if (resultCode == RESULT_OK &&
+                requestCode == REQUEST_CODE_CONFIRMACAO) {
+            if (dados.hasExtra("retorno")) {
+                Toast.makeText(this, "Retornou", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     private ArrayList<Evento> adicionarEvento() {
         ArrayList<Evento> evento = new ArrayList<Evento>();
-        Evento ev1 = new Evento("Show Ed Sheeran", "26/04/17", "Alianz Arena", 45000, "", "", 345.00, R.drawable.nome);
+        Evento ev1 = new Evento("Botafogo x Cruzeiro", "03/12/17", "Rio de Janeiro, Estadio do Engenhão", 45000, "CBF", "", 50.00, R.drawable.nome);
         evento.add(ev1);
-
-
+        Evento ev2 = new Evento("Vasco x Ponte Preta", "03/12/17", "Rio de Janeiro, São Januário", 30000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev2);
+        Evento ev3 = new Evento("Santos x Avaí", "03/12/17", "Santos, Vila Belmiro", 18000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev3);
+        Evento ev4 = new Evento("São Paulo x Bahia", "03/12/17", "São Paulo, Estádio do Morumbi", 65000, "CBF", "", 10.00, R.drawable.nome);
+        evento.add(ev4);
+        Evento ev5 = new Evento("Atlético-MG x Grêmio", "03/12/17", "Belo Horizonte, Estádio Independência", 18000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev5);
+        Evento ev6 = new Evento("Atlética-PR x Palmeiras", "03/12/17", "Curitiba, Arena da Baixada", 38000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev6);
+        Evento ev7 = new Evento("Sport x Corinthians", "03/12/17", "Recife, Ilha do Retiro", 18000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev7);
+        Evento ev8 = new Evento("Vitória x Flamengo", "03/12/17", "Salvador, Estádio Barradão", 25000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev8);
+        Evento ev9 = new Evento("Atlético-GO x Fluminense", "03/12/17", "Goiânia, Estádio Serra Dourada", 39000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev9);
+        Evento ev10 = new Evento("Chapecoense x Coritiba", "03/12/17", "Chapecó, Arena Condá", 25000, "CBF", "", 50.00, R.drawable.nome);
+        evento.add(ev10);
         return evento;
     }
 }
